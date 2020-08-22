268b7434aa545837607ffec4101390e45f47ffcb
Maciek Sitkowski <msitkowski94@gmail.com>
2020-08-22 14:53:43 +0200

    add css-variables

---
+++ src/sections/Hero.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Hero.module.scss';
+
+export default function Hero() {
+  return (
+    <div className={classes.wrapper}>
+      
+    </div>
+  )
+}
