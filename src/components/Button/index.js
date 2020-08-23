fa1a64930a924ca95ab808239ad399a83d9626ff
Maciek Sitkowski <msitkowski94@gmail.com>
2020-08-23 15:48:50 +0200

    Add className prop to button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './button.scss';
 import cx from 'classnames';
 
-export default function Button({ white, green, animated, children, ...props }) {
+export default function Button({ className, white, green, animated, children, ...props }) {
   const className = cx({
     "btn": true,
     "btn--white": white,
     "btn--green": green,
-    "btn--animated": animated
+    "btn--animated": animated,
+    className
   })
 
   return (
